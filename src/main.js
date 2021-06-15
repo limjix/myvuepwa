7a5f1ba4c454d3496b612598724d1abcba239a7a
limjix <limjix@gmail.com>
2021-06-15 21:16:13 +0800

    After 'vue add pwa'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(vuetify)
